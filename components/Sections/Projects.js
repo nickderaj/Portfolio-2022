bb1aa652555afeec1b1c198e00c1e13a6c6ac11c
nickderaj <nickderaj@gmail.com>
2022-05-25 23:59:57 +0800

    Move Octocat to paragraph

--- components/Sections/Projects.js
+++ components/Sections/Projects.js
@@ -7,19 +7,27 @@ export default function Projects() {
         <h2 className="text-4xl sm:text-6xl font-bold text-rose-500 font-kalam pb-0 lg:pb-6">
           Recent Projects
         </h2>
-        <a
+        {/* <a
           href="https://github.com/nickderaj"
           target="_blank"
           rel="noreferrer"
           className="absolute top-12 right-12 lg:right-40 cursor-pointer wiggle"
         >
           <Image src="/svg/octocat.svg" alt="robot" width="50" height="50" />
-        </a>
+        </a> */}
         <div className="h-full w-full flex flex-col items-center font-kalam text-lg gap-6 pt-20">
           <Image src="/svg/saw.svg" width="100" height="100" alt="Maintenance" />
-          <p className="px-6 text-center">
+          <p className="px-6 text-center relative">
             Section under construction, click the Octocat to view Github!{' '}
             <span className="text-2xl">&#8605;</span>
+            <a
+              href="https://github.com/nickderaj"
+              target="_blank"
+              rel="noreferrer"
+              className="absolute -top-12 -right-4 cursor-pointer wiggle"
+            >
+              <Image src="/svg/octocat.svg" alt="robot" width="50" height="50" />
+            </a>
           </p>
         </div>
       </div>
